e2d1a1f29d2ec2ac59b84e43c511ddb2479a1d06
Danielle Jasper <dnj547@live.com>
2019-03-13 23:51:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,6 @@ def get_first_name_of_season_winner(data, season)
     if season_key == season
       all_contestant_info.each do |contestant_info|
         contestant_info.each do |info_key, info_value|
-                      binding.pry
           if info_key[info_value] == "Winner"
             return contestant_info["name"].split[0]
           end
