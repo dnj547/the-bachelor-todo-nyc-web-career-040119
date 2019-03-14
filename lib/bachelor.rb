fe675a9799f2c24777bb803d9b1ba340cb899443
Danielle Jasper <dnj547@live.com>
2019-03-14 00:19:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_key, all_contestant_info|
     if season_key == season
       all_contestant_info.each do |contestant_info|
-        ages << contestant_info["age"]
+        ages << contestant_info["age"].to_i
       end
     end
   end
